3254904364b5ce9c424fb283e4436083f4a7d0aa
Pavlo Starobub <starodub.pavlo@alsodev.md>
2025-12-19 15:12:13 +0200

    Add add & add_all

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from sqlalchemy.orm import sessionmaker
 from models import User, engine
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+user = User(name="John", age=30)
+user_2 = User(name="Jane", age=25)
+user_3 = User(name="Bob", age=20)
+user_4 = User(name="Alice", age=22)
+session.add_all([user_3, user_4])
+session.add(user_2)
+session.commit()
